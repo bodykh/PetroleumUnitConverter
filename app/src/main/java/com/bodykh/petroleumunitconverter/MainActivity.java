4cce1dba53c4fb53d8d12e6a263745cc3bb7f57b
bodykh <bodyono3@gmail.com>
2020-11-14 17:28:22 +0200

    Initial commit

---
+++ app/src/main/java/com/bodykh/petroleumunitconverter/MainActivity.java
@@ -0,0 +1,42 @@
+package com.bodykh.petroleumunitconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void volume_but(View view) {
+        Intent intent1 = new Intent(MainActivity.this, volumeconv.class);
+        startActivity(intent1);
+    }
+
+    public void area_but(View view) {
+        Intent intent2 = new Intent(MainActivity.this, areaconv.class);
+        startActivity(intent2);
+    }
+
+    public void length_but(View view) {
+        Intent intent3 = new Intent(MainActivity.this, lengthconv.class);
+        startActivity(intent3);
+    }
+
+    public void pressure_but(View view) {
+        Intent intent4 = new Intent(MainActivity.this, pressureconv.class);
+        startActivity(intent4);
+    }
+
+    public void temperature_but(View view) {
+        Intent intent5 = new Intent(MainActivity.this, temperatureconv.class);
+        startActivity(intent5);
+    }
+}
---
+++ app/src/main/java/com/bodykh/petroleumunitconverter/lengthconv.java
@@ -0,0 +1,14 @@
+package com.bodykh.petroleumunitconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class lengthconv extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lengthconv);
+    }
+}
